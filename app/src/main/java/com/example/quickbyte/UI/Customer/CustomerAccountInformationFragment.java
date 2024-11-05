c1116aebd11154f6d9f5f0a4f013e37520121461
jbufufna <83720728+jbufufna@users.noreply.github.com>
2024-11-05 12:06:09 -0500

    Half of the customer pages java files created

--- app/src/main/java/com/example/quickbyte/UI/Customer/CustomerAccountInformationFragment.java
+++ app/src/main/java/com/example/quickbyte/UI/Customer/CustomerAccountInformationFragment.java
@@ -1,4 +1,4 @@
-package com.example.quickbyte;
+package com.example.quickbyte.UI.Customer;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.quickbyte.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerCreateAccountFragment#newInstance} factory method to
+ * Use the {@link CustomerAccountInformationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerCreateAccountFragment extends Fragment {
+public class CustomerAccountInformationFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CustomerCreateAccountFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerCreateAccountFragment() {
+    public CustomerAccountInformationFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CustomerCreateAccountFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerCreateAccountFragment.
+     * @return A new instance of fragment CustomerAccountInformationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerCreateAccountFragment newInstance(String param1, String param2) {
-        CustomerCreateAccountFragment fragment = new CustomerCreateAccountFragment();
+    public static CustomerAccountInformationFragment newInstance(String param1, String param2) {
+        CustomerAccountInformationFragment fragment = new CustomerAccountInformationFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CustomerCreateAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_create_account, container, false);
+        return inflater.inflate(R.layout.customer_account_information, container, false);
     }
 }
--- app/src/main/java/com/example/quickbyte/UI/Customer/CustomerHomePageFragment.java
+++ app/src/main/java/com/example/quickbyte/UI/Customer/CustomerHomePageFragment.java
@@ -1,4 +1,4 @@
-package com.example.quickbyte;
+package com.example.quickbyte.UI.Customer;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.quickbyte.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerAccountInformationFragment#newInstance} factory method to
+ * Use the {@link CustomerHomePageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerAccountInformationFragment extends Fragment {
+public class CustomerHomePageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CustomerAccountInformationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerAccountInformationFragment() {
+    public CustomerHomePageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CustomerAccountInformationFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerAccountInformationFragment.
+     * @return A new instance of fragment CustomerHomePageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerAccountInformationFragment newInstance(String param1, String param2) {
-        CustomerAccountInformationFragment fragment = new CustomerAccountInformationFragment();
+    public static CustomerHomePageFragment newInstance(String param1, String param2) {
+        CustomerHomePageFragment fragment = new CustomerHomePageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CustomerAccountInformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_account_information, container, false);
+        return inflater.inflate(R.layout.customer_home_page, container, false);
     }
 }
