0acfecf802a9ac2ceb081f957a8a63fcc61ef1c1
JoelC-umich <joelcord@umich.edu>
2024-11-27 16:08:20 -0500

    Finished with basic UI changes
    Done with basic UI changes. Need to change some buttons and all functionality (saving data to variables.

--- app/src/main/java/com/example/quickbyte/UI/Business/BusinessModifyBusinessFragment.java
+++ app/src/main/java/com/example/quickbyte/UI/Business/BusinessModifyBusinessFragment.java
@@ -2,21 +2,35 @@ package com.example.quickbyte.UI.Business;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.quickbyte.R;
+import com.example.quickbyte.databinding.BusinessModifyBusinessBinding;
 
 
 public class BusinessModifyBusinessFragment extends Fragment {
 
+    private BusinessModifyBusinessBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.business_modify_business, container, false);
+        binding = com.example.quickbyte.databinding.BusinessModifyBusinessBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnBizInfoBack.setOnClickListener(v ->
+                NavHostFragment.findNavController(BusinessModifyBusinessFragment.this)
+                        .navigate(R.id.action_businessModifyBusinessFragment_to_businessIncomingOrdersFragment)
+        );
     }
 }
