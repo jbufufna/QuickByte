dad5e9bd4cd849ff4fa4ce4709bc3793376d0a7b
dipanshu_tihal_ <dtihal.dipanshu@gmail.com>
2024-12-05 20:43:54 -0500

    Updated UserService class

--- app/src/main/java/com/example/quickbyte/API/Services/UserService.java
+++ app/src/main/java/com/example/quickbyte/API/Services/UserService.java
@@ -218,10 +218,6 @@ public class UserService {
         });
     }
 
-    // Add other methods for updateUser, deleteUser, etc.
-
-
-
     public interface ApiCallback<T> {
         void onSuccess(T result);
         void onError(String errorMessage);
