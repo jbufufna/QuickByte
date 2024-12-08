3e516c5cb9b44ef6e34065fe66184f524886d149
dipanshu_tihal_ <dtihal.dipanshu@gmail.com>
2024-12-07 20:59:10 -0500

    Updated UserDTO for pass

--- app/src/main/java/com/example/quickbyte/API/DTO/UserDTO.java
+++ app/src/main/java/com/example/quickbyte/API/DTO/UserDTO.java
@@ -6,6 +6,7 @@ public class UserDTO {
     private Integer userId;
     private String username;
     private String email;
+    private String passwordHash;
     private String firstName;
     private String lastName;
     private String phoneNumber;
@@ -22,10 +23,11 @@ public class UserDTO {
 
     // Constructor with all fields
     public UserDTO(Integer userId, String username, String email, String firstName, String lastName,
-                   String phoneNumber, LocalDateTime createdAt, Boolean isActive, int LoyaltyPoints, String CardNumber, int ExpiryMonth, int ExpiryYear, Boolean IsDefaultCard, int cvv) {
+                   String phoneNumber, LocalDateTime createdAt, Boolean isActive, int LoyaltyPoints, String CardNumber, int ExpiryMonth, int ExpiryYear, Boolean IsDefaultCard, int cvv, String passwordHash) {
         this.userId = userId;
         this.username = username;
         this.email = email;
+        this.passwordHash = passwordHash;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
@@ -62,6 +64,14 @@ public class UserDTO {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
     }
 
     public String getFirstName() {
